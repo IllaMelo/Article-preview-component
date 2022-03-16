23f382d375f4d1e9ec62f9ec25ba2713b2ff4325
IllaMelo <dantas.mill.melo@gmail.com>
2022-03-16 18:59:10 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const classOn = document.querySelector(".author-principal-on");
-const classOff=document.querySelector(".links-off");
-function Changeclass(){
-    if (classOn==".author-principal-on"){
-        classOn.classList.replace(".author-principal-on",".author-principal-off");
-        classOff.classList.replace(".links-off",".links-on");
-       
-    }
-    else{
-        classOn.classList.replace(".author-principal-off",".author-principal-on");
-        classOff.classList.replace(".links-on",".links-off");
-    }
+(function(){
+
+
+document.querySelector(".links-on").onclick = function (){
+    document.querySelector(".links-off").style.display = "flex";
+    document.querySelector(".links-on").style.display = "none";
 }
+document.querySelector(".links-off").onclick = function (){
+    document.querySelector(".links-on").style.display = "flex";
+    document.querySelector(".links-off").style.display = "none";
+}
+
+})();
